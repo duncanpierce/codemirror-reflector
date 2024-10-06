b45dc2587578dc012102fdadcd114a9a1540a261
Duncan Pierce <duncan@duncanpierce.org>
2024-10-06 20:51:24 +0100

    Switch to defaultKeymap

--- example/main.ts
+++ example/main.ts
@@ -1,7 +1,7 @@
 import { drawSelection, EditorView, keymap } from "@codemirror/view"
 import { EditorState } from "@codemirror/state"
 import { autocompletion, closeBracketsKeymap, completionKeymap } from "@codemirror/autocomplete"
-import { standardKeymap } from "@codemirror/commands"
+import { defaultKeymap } from "@codemirror/commands"
 import { lintGutter, lintKeymap } from "@codemirror/lint"
 
 const editorElement = document.querySelector('#editor')!
@@ -38,12 +38,10 @@ let editorView = new EditorView({
             lintGutter(),
             drawSelection(), // include this to allow Reflector to show multiple selections; TODO include in Reflector extension
             EditorState.allowMultipleSelections.of(true), // TODO include in Reflector extension
-            autocompletion({
-                selectOnOpen: false,
-            }),
+            autocompletion(),
             keymap.of([
                 ...closeBracketsKeymap,
-                ...standardKeymap,
+                ...defaultKeymap,
                 ...completionKeymap,
                 ...lintKeymap,
             ]),
